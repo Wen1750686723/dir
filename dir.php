224542af2c93ab23dc1814b5a2becd09225c2953
Wen1750686723 <wen1750686723@qq.com>
2015-06-24 16:42:12 +0800

    first commit

---
+++ dir.php
@@ -0,0 +1,39 @@
+<?php
+function getDir($dir) {
+    $dirArray[]=NULL;
+    if (false != ($handle = opendir ( $dir ))) {
+        $i=0;
+        while ( false !== ($file = readdir ( $handle )) ) {
+            //去掉"“.”、“..”以及带“.xxx”后缀的文件
+            if ($file != "." && $file != ".."&&!strpos($file,".")) {
+                $dirArray[$i]=$file;
+                $i++;
+            }
+        }
+        //关闭句柄
+        closedir ( $handle );
+    }
+    return $dirArray;
+}
+function getFile($dir) {
+    $fileArray[]=NULL;
+    if (false != ($handle = opendir ( $dir ))) {
+        $i=0;
+        while ( false !== ($file = readdir ( $handle )) ) {
+            //去掉"“.”、“..”以及带“.xxx”后缀的文件
+            if ($file != "." && $file != ".."&&strpos($file,".")) {
+                $fileArray[$i]="./imageroot/current/".$file;
+                if($i==100){
+                    break;
+                }
+                $i++;
+            }
+        }
+        //关闭句柄
+        closedir ( $handle );
+    }
+    return $fileArray;
+}
+var_dump(getDir("../"));
+var_dump(getFile("../"));
+?>
